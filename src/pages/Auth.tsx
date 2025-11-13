c1deae557fce012da5885a7f806ce9bd587ef3f3
Swikar Thanekar <swikarthanekar@gmail.com>
2025-11-13 14:13:45 +0530

    feat(auth): implement auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import {
-  Card,
+  Card, 
   CardContent,
   CardDescription,
   CardHeader,
